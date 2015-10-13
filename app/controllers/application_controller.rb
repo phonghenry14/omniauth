9a3252c4da7f581263875845b4f301809b79e1ad
phonghenry14 <tran.duc.phong@framgia.com>
2015-10-13 15:02:54 +0700

    demo omniauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  before_filter :ensure_signup_complete, only: [:new, :create, :update, :destroy]
+  before_action :authenticate_user!
+
   protect_from_forgery with: :exception
+
+  def ensure_signup_complete
+    return if action_name == "finish_signup"
+    if current_user && !current_user.email_verified?
+      redirect_to finish_signup_path(current_user)
+    end
+  end
 end
